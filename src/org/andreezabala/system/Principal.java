c8e41c58bc972be08702e7b3fd8da5fae9027f40
AndyAjiatas07 <aajiatas-2021496@kinal.edu.gt>
2026-06-05 10:48:18 -0600

    006 se agrefo el nombre en el hola mundo

--- src/org/andreezabala/system/Principal.java
+++ src/org/andreezabala/system/Principal.java
@@ -8,7 +8,7 @@ package org.andreezabala.system;
 public class Principal {
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo Mi Nombre es Ronald");
     }
     
 }
